f9cb5b0ca69667fa452babbb689edd1184cf29d4
suisen <205521266+suisenzoku@users.noreply.github.com>
2025-04-02 15:56:48 -0500

    Separate cell creation

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const grid = document.querySelector(".grid");
 const WIDTH = 600;
 
+const createCell = (size) => {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.width = size;
+    cell.style.height = size;
+    return cell;
+}
+
 const fillGrid = (size) => {
     const cellSize = `${WIDTH / size}px`;
     for (let i = 0; i < size * size; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.style.width = cellSize;
-        cell.style.height = cellSize;
+        const cell = createCell(cellSize);
         grid.append(cell);
     }
 }
